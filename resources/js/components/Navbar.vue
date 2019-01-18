b6bb031aa547e13d0440c6cba58cc9ae7fdd32bd
Bayarsaikhan <https://github.com/Bayarsaikhan3033>
2019-01-19 00:59:12 +0800

    vue component

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-2">
+    <div class="container">
+      <a href="#" class="navbar-brand">Article APP</a>
+    </div>
+  </nav>
+</template>
